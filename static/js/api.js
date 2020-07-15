c0fcb648b3e2ace2b330ec5ae19de517a52c8f81
Aron Arias <aron.ar.96@gmail.com>
2020-07-15 00:42:15 -0500

    Codigo sin comentarios

--- static/js/api.js
+++ static/js/api.js
@@ -3,7 +3,5 @@ export default class API{
         const response=await fetch(`https://rickandmortyapi.com/api/character/${id}`)
         const data=await response.json()
         return data
-        // .then((response)=>response.json())
-        // .then((data)=>data)
     }
 }
